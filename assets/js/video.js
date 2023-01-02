f36adae619ee1e1ce95ddcdbda36e4a50d000ff0
Goldnboy26 <enrique.monje1994@yahoo.com>
2023-01-01 21:56:13 -0800

    scroll to top btn

--- assets/js/video.js
+++ assets/js/video.js
@@ -12,3 +12,16 @@ function stopVideo(){
     myVideo.src=file;
     videoPlayer.style.display="block";
   }
+
+  const header = document.querySelector("[data-header]");
+  const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
