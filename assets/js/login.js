f3920e080ca4f952310daf1652e25fd96de9e9d6
Goldnboy26 <enrique.monje1994@yahoo.com>
2023-01-19 22:36:48 -0800

    detectmobile

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,9 +1,1 @@
-const ua = detect.parse(navigator.userAgent)
-const button = document.querySelector('login')
 
-if (ua.device !==null){
-if(ua.device.family=== 'iPhone'){
-    button.textContent= 'login'
-    button.href = 'https://goldnboy26.github.io/EMFITNESS/gallery.html'
-  }
-}
