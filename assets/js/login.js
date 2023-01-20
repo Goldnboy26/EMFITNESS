47907b035548478fbabc1f01944b3dd501d9ae49
Goldnboy26 <enrique.monje1994@yahoo.com>
2023-01-19 21:32:53 -0800

    mobile view

--- assets/js/login.js
+++ assets/js/login.js
@@ -7,15 +7,5 @@ $('#detect-button').click(function(){
     console.log( "Tablet: " + detector.tablet());
     console.log( "OS: " + detector.os());
     console.log( "userAgent: " + detector.userAgent());
-
-    if (email== "enrique.monje1994@yahoo.com" && password=="goldnboy26"){
-        window.location.href= 'https://goldnboy26.github.io/EMFITNESS/gallery.html'
-        alert("login Successfully");
-    }
-
-    else{
-        alert("Invalid Information")
-        return;
-    }
     
   });
