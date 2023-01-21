c4ddcec60e4c489bbb94a9931e13aed641ac143d
Goldnboy26 <enrique.monje1994@yahoo.com>
2023-01-20 17:02:47 -0800

    detectmobile

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,9 +1,10 @@
 function auth() {
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
+    var login = document.getElementById("login")
 
-    if(screen.width <= 699){
-        window.location.href = 'https://goldnboy26.github.io/EMFITNESS/gallery.html';
+    if(screen.width <= 699 || email== "admin@gmail.com" && password=="admin123" ){
+        window.redirect.href = 'https://goldnboy26.github.io/EMFITNESS/gallery.html';
     }
 
     if (email== "admin@gmail.com" && password=="admin123" ){
