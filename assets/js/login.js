59299b863d30b4f12b703d3c951b4e1d0bcaa8fb
Goldnboy26 <enrique.monje1994@yahoo.com>
2023-01-23 23:07:36 -0800

    login

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,22 +1,2 @@
-function auth() {
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
 
-    if(screen.width <= 640 || email== "admin@gmail.com" && password=="admin123" 
-    || email== "enrique.monje1994@yahoo.com" && password=="goldnboy26"){
-        window.location.href = 'https://goldnboy26.github.io/EMFITNESS/gallery.html'
-        alert("login Successfully");
-    }
-
-    if (email== "admin@gmail.com" && password=="admin123" 
-    || email== "enrique.monje1994@yahoo.com" && password=="goldnboy26"){
-        window.location.href= 'https://goldnboy26.github.io/EMFITNESS/gallery.html'
-        alert("login Successfully");
-    }
-
-    else{
-        alert("Invalid Information")
-        return;
-    }
-}
   
