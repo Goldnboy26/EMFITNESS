84fe552d7a45c3910ecf6edfaff5d4bfa61102af
Goldnboy26 <enrique.monje1994@yahoo.com>
2023-01-26 00:08:45 -0800

    login bug

--- assets/js/login.js
+++ assets/js/login.js
@@ -30,3 +30,16 @@ function signIn(){
   document.body.appendChild(form);
   form.submit();
 }
+  
+function logout() {
+  fetch("https://oauth2.googleapis.com/revoke?token=" + info['access_token'],
+      {
+          method: 'POST',
+          headers: {
+              "Content-type": "application/x-www-form-urlencoded"
+          }
+      })
+      .then((data) => {
+          location.href = "https://goldnboy26.github.io/EMFITNESS"
+      })
+}
